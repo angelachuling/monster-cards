dee259c474408547ea3c14b96957f7809e1fb3ef
angelachuling <angelachuling@hotmail.com>
2020-10-15 17:12:32 +0200

    Updates

---
+++ precache-manifest.0fd7f62f54e62d766b41e8d95966d90b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d9e397e00b42fcd22c11f9ce9f50ec",
+    "url": "/monster-cards/index.html"
+  },
+  {
+    "revision": "1fbe75ef7101933921df",
+    "url": "/monster-cards/static/css/main.d8f5ba1d.chunk.css"
+  },
+  {
+    "revision": "e4f25e371f18ae9db564",
+    "url": "/monster-cards/static/js/2.0e0a43ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-cards/static/js/2.0e0a43ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fbe75ef7101933921df",
+    "url": "/monster-cards/static/js/main.bf6381fc.chunk.js"
+  },
+  {
+    "revision": "0603b0a5437a87d7932c",
+    "url": "/monster-cards/static/js/runtime-main.6afd87db.js"
+  }
+]);
